0d5fd8ed8c63be8fd1ddd921fddefaf157df635d
ardaaraz <ardaaraza@gmail.com>
2021-03-29 13:52:57 +0300

    Implement PID controller

--- src/main.cpp
+++ src/main.cpp
@@ -37,6 +37,12 @@ int main() {
   /**
    * TODO: Initialize the pid variable.
    */
+  
+  // Controller Parameters
+  double Kp = 0.12;
+  double Ki = 0.001;
+  double Kd = 2.5;
+  pid.Init(Kp, Ki, Kd);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
                      uWS::OpCode opCode) {
@@ -63,6 +69,13 @@ int main() {
            * NOTE: Feel free to play around with the throttle and speed.
            *   Maybe use another PID controller to control the speed!
            */
+          pid.UpdateError(cte);
+          steer_value = pid.TotalError();
+          // Saturation
+          if(steer_value < -1)
+            steer_value = -1;
+          else if(steer_value > 1)
+            steer_value = 1;
           
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value 
